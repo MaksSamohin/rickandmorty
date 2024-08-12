a92fb60818c86b0529703a242207c3bfa96178ba
MaksSamohin <user.name>
2024-08-12 12:19:43 +0300

    fix: fixed showlabel issue in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,29 +1,13 @@
-import { Typography, BottomNavigation } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import styles from "./Footer.module.css";
-import { styled } from "@mui/material";
-
-const CustomBottomNavigation = styled(BottomNavigation)({
-  backgroundColor: "white",
-  fontFamily: '"Karla", sans-serif',
-  boxShadow: "0 5px 10px 5px rgba(0, 0, 0, .05)",
-  color: "black",
-  marginTop: "20px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  width: "100%",
-  height: "56px",
-  zIndex: "1000",
-  textAlign: "center",
-});
 
 function Footer() {
   return (
-    <CustomBottomNavigation className={styles.footer}>
+    <Box className={styles.footer}>
       <Typography className={styles.footerText}>
         Make with ❤️ for the MobProgramming team
       </Typography>
-    </CustomBottomNavigation>
+    </Box>
   );
 }
 
